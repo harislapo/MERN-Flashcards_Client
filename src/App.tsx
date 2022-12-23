1e64d3fa768af945817750a5701dd641db4fe7a1
harislapo <haris.lapo@gmail.com>
2022-12-23 13:10:54 +0100

    Added input for adding new flashcards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const [title, setTitle] = useState('');
+
+  const handleAddDeck = (e: React.FormEvent) => {
+    e.preventDefault();
+    fetch('http://localhost:5000/decks', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+      }),
+    });
+  };
+
+  return (
+    <div className="App">
+      <form onSubmit={handleAddDeck}>
+        <label htmlFor="deck-title">Title</label>
+        <input
+          type="text"
+          id="deck-title"
+          value={title}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setTitle(e.target.value);
+          }}
+        />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
